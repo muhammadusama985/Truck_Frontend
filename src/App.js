e91db96e203bbdf03698f42d05e76e92f1c4154f
Muhammad Usama <150240053+muhammadusama985@users.noreply.github.com>
2025-04-13 01:09:44 +0500

    Update App.js again live

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-// // src/App.js
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Signup from './new/Landing_Page/Signup';
-// import Signin from './new/Landing_Page/Signin';
-// import MainPage from './new/Landing_Page/MainPage';
-// import DashboardLayout from './new/Admin_Dashboard/DashboardLayout';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './new/Landing_Page/Signup';
+import Signin from './new/Landing_Page/Signin';
+import MainPage from './new/Landing_Page/MainPage';
+import DashboardLayout from './new/Admin_Dashboard/DashboardLayout';
 
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-//         <Routes>
-//           {/* Define the route for the Leased Carrier Login */}
-//           <Route path="/" element={<MainPage />} />
-//           <Route path="/Signup" element={<Signup />} />
-//           <Route path="/Signin" element={<Signin />} />
-//           <Route path="/Dashboard" element={<DashboardLayout />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          {/* Define the route for the Leased Carrier Login */}
+          <Route path="/" element={<MainPage />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Dashboard" element={<DashboardLayout />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
